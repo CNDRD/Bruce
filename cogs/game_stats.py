123e6074a7eea6ab090728b16e1ad304464b27d1
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-03-29 15:48:50 +0200

    only start loops that were running

--- cogs/game_stats.py
+++ cogs/game_stats.py
@@ -37,7 +37,7 @@ class GameStats(commands.Cog):
 
     @tasks.loop(minutes=dbcsgo_loop_time)
     async def dbcsgo(self):
-        cl('', 'GameStats', 'dbcsgo')
+        cl('', 'GameStats', 'dbcsgo loop')
         users = db.child('GameStats').child('IDs').get()
         for u in users.each():
             if (steam_id_32 := u.val().get('steamID32')) is not None:
@@ -49,7 +49,7 @@ class GameStats(commands.Cog):
 
     @tasks.loop(minutes=dbr6_loop_time)
     async def dbr6(self):
-        cl('', 'GameStats', 'dbr6')
+        cl('', 'GameStats', 'dbr6 loop')
         users = db.child('GameStats').child('IDs').get()
         for u in users.each():
             if (ubi_id := u.val().get('ubiID')) is not None:
@@ -63,12 +63,14 @@ class GameStats(commands.Cog):
     async def stats_update(self, ctx):
         cl(ctx)
         # Stops and then prompltly starts all stats loops
-        self.dbcsgo.cancel()
-        self.dbr6.cancel()
+        if dbcsgo_loop: self.dbcsgo.cancel()
+        if dbr6_loop: self.dbr6.cancel()
         # This 0.5s delay needs to be here because who the fuck knows why
         await asyncio.sleep(0.5)
-        self.dbcsgo.start()
-        self.dbr6.start()
+
+        if dbcsgo_loop: self.dbcsgo.start()
+        if dbr6_loop: self.dbr6.start()
+
         await ctx.message.add_reaction('✅')
 
 
