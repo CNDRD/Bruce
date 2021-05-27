7660dddd094efccf1413279f0dd7cdfe3c389794
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-05-28 01:48:37 +0200

    it's too fucking late for this shit bro

--- bot.py
+++ bot.py
@@ -20,10 +20,11 @@ db_auto_backup_loop = config.get('db_auto_backup_loop')
 token = os.getenv('bruce_token')
 
 ## Firebase ##
-config = {"apiKey": "AIzaSyDe_xKKup4lVoPasLmAQW9Csc1zUzsxB0U","authDomain": "chuckwalla-69.firebaseapp.com",
+firebase_config = {"apiKey": "AIzaSyDe_xKKup4lVoPasLmAQW9Csc1zUzsxB0U","authDomain": "chuckwalla-69.firebaseapp.com",
   "databaseURL": "https://chuckwalla-69.firebaseio.com","storageBucket": "chuckwalla-69.appspot.com",
-  "serviceAccount": json.loads(os.getenv("serviceAccountKeyJSON"))}
-db = pyrebase.initialize_app(config).database()
+  "serviceAccount": json.loads(os.getenv("SERVICE_ACCOUNT"))}
+firebase_config["serviceAccount"]["private_key"] = f'-----BEGIN PRIVATE KEY-----{firebase_config["serviceAccount"]["private_key"]}-----END PRIVATE KEY-----\n'
+db = pyrebase.initialize_app(firebase_config).database()
 
 ## Basic Bot Setup ##
 client = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
