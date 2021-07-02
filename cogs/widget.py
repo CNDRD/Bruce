b3edd95f7cdb3476c3878725faa65d854b3857e6
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-07-02 16:57:39 +0200

    remove member color, get member data from function, -spotify cover url

--- cogs/widget.py
+++ cogs/widget.py
@@ -30,18 +30,8 @@ class Widget(commands.Cog):
 
         for member in diskito.members:
             if not member.bot:
-                xd = {
-                    "uid": member.id,
-                    "username": member.name,
-                    "house": getHypesquadHouse(member.public_flags),
-                    "voice": getVoice(member.voice),
-                    "premium_since": getPremium(member.premium_since),
-                    "status": getStatus(member.raw_status),
-                    "is_on_mobile": member.is_on_mobile(),
-                    "color": getColor(member.color.to_rgb()),
-                    "activities": getActivities(member.activities),
-                }
-                widgeee[member.id] = xd
+                widgeee[member.id] = gimmeThemStats(member)
+
         db.child("widget").update(widgeee)
 
 
@@ -50,17 +40,7 @@ class Widget(commands.Cog):
         if after.guild.id != diskito_id: return
         if after.bot: return
 
-        x = {
-            "uid": after.id,
-            "username": after.name,
-            "house": getHypesquadHouse(after.public_flags),
-            "voice": getVoice(after.voice),
-            "premium_since": getPremium(after.premium_since),
-            "status": getStatus(after.raw_status),
-            "is_on_mobile": after.is_on_mobile(),
-            "color": getColor(after.color.to_rgb()),
-            "activities": getActivities(after.activities),
-        }
+        x = gimmeThemStats(after)
 
         db.child("widget").child(x["uid"]).update(x)
 
@@ -69,6 +49,18 @@ def setup(client):
     client.add_cog(Widget(client))
 
 
+def gimmeThemStats(mem):
+    x = {
+        "uid": mem.id,
+        "username": mem.name,
+        "house": getHypesquadHouse(mem.public_flags),
+        "voice": getVoice(mem.voice),
+        "premium_since": getPremium(mem.premium_since),
+        "status": getStatus(mem.raw_status),
+        "is_on_mobile": mem.is_on_mobile(),
+        "activities": getActivities(mem.activities),
+    }
+    return x
 def getHypesquadHouse(flags):
     if flags.hypesquad_balance:
         return "balance"
@@ -77,7 +69,6 @@ def getHypesquadHouse(flags):
     if flags.hypesquad_brilliance:
         return "brilliance"
     return "none"
-
 def getVoice(v):
     xd = {
         'deaf': False, 'mute': False,
@@ -102,7 +93,6 @@ def getVoice(v):
         xd['self_video'] = True
 
     return xd
-
 def getActivities(activities):
     if activities == (): return "none"
 
@@ -117,7 +107,6 @@ def getActivities(activities):
             xd["spotify"] = {
                 "artist": activity.artists[0],
                 "title": activity.title,
-                "album_cover_url": activity.album_cover_url,
             }
 
         elif isinstance(activity, discord.activity.CustomActivity):
@@ -140,14 +129,9 @@ def getActivities(activities):
             xd["other"].append(str(activity.name))
 
     return xd
-
 def getPremium(uhh):
     if uhh is None: return "none"
     return datetime.timestamp(uhh)
-
-def getColor(xd):
-    return {"r": xd[0], "g": xd[1], "b": xd[2]}
-
 def getStatus(s):
     if not isinstance(s, str):
         return s[0]
