d1b3b5d4ebb282d3a2211958f0fa5c06bc91ea2b
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-08-24 01:05:55 +0200

    + apex staaaaaaaaattsssssss

--- cogs/game_stats.py
+++ cogs/game_stats.py
@@ -1,4 +1,5 @@
 from func.r6 import rainbow6stats
+from func.apex import apexStats
 from func.firebase_init import db
 
 import pyrebase, yaml, json, asyncio, time, os
@@ -11,11 +12,13 @@ nest_asyncio.apply()
 ## Config Load ##
 config = yaml.safe_load(open('config.yml'))
 bot_mod_role_id = config.get('bot_mod_role_id')
-slash_guilds = config.get('slash_guilds')
 dbr6_loop = config.get('dbr6_loop')
 dbr6_loop_time = config.get('dbr6_loop_time')
+apex_loop = config.get('apex_loop')
+apex_loop_time = config.get('apex_loop_time')
 
 R6STATS_VERSION = 9
+APEX_VERSION = 1
 
 class GameStats(commands.Cog):
     def __init__(self, client):
@@ -23,11 +26,13 @@ class GameStats(commands.Cog):
         Various Game Stats gathering loops.
 
         - dbr6 (loop)
+        - apex (loop)
         - stats_update
         """
         self.client = client
         self.first_boot = True
         if dbr6_loop: self.dbr6.start()
+        if apex_loop: self.apex.start()
 
         self.last_siege_update_ts = db.child("GameStats").child("lastUpdate").child(f"R6Sv{R6STATS_VERSION}").get().val()
         def wrtus(message):
@@ -61,16 +66,29 @@ class GameStats(commands.Cog):
         db.child('GameStats').child(f'R6Sv{R6STATS_VERSION}').update(data)
         db.child('GameStats').child('lastUpdate').update({f'R6Sv{R6STATS_VERSION}':int(time.time())})
 
-
     @dbr6.before_loop
     async def before_dbr6(self):
         print('dbr6 - wait_until_ready()')
         await self.client.wait_until_ready()
 
-
     @dbr6.after_loop
     async def after_dbr6(self):
-        print("R6 Loop Done")
+        print("dbr6 - Loop Done")
+
+
+    @tasks.loop(minutes=apex_loop_time)
+    async def apex(self):
+        ape_sex_stats = apexStats()
+        db.child('GameStats').child(f'ApexV{APEX_VERSION}').update(ape_sex_stats)
+
+    @apex.before_loop
+    async def before_apex(self):
+        print('apex - wait_until_ready()')
+        await self.client.wait_until_ready()
+
+    @apex.after_loop
+    async def after_apex(self):
+        print("apex - Loop Done")
 
 
     @commands.command(aliases=['su'])
@@ -78,9 +96,11 @@ class GameStats(commands.Cog):
     async def stats_update(self, ctx):
         # Stops and then prompltly starts all stats loops
         if dbr6_loop: self.dbr6.cancel()
+        if apex_loop: self.apex.cancel()
         # This 0.5s delay needs to be here because who the fuck knows why
         await asyncio.sleep(0.5)
         if dbr6_loop: self.dbr6.start()
+        if apex_loop: self.apex.start()
         await ctx.message.add_reaction('✅')
 
 
