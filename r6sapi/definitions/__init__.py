155b12703513e5c6d2f98d8088c3d312aa39ce60
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-09-23 19:33:58 +0200

    removed r6sapi because i made it pip installable

--- r6sapi/definitions/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from r6sapi.definitions.stores import Loadouts, Operators, Seasons
-from r6sapi.definitions.loadouts import loadouts_const
-from r6sapi.definitions.operators import operators_const
-from r6sapi.definitions.seasons import seasons_const
-
-loadouts = Loadouts(loadouts_const)
-operators = Operators(operators_const, loadouts)
-seasons = Seasons(seasons_const)
--- r6sapi/exceptions.py
+++
@@ -1,8 +0,0 @@
-
-class InvalidRequest(Exception):
-    def __init__(self, *args, code=0, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.code = code
-
-
-class FailedToConnect(Exception): pass
