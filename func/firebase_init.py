cbe46570d3442d83767b603e2017fa561dcf136a
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-09-27 00:54:44 +0200

    easier switching when developing

--- func/firebase_init.py
+++ func/firebase_init.py
@@ -7,4 +7,5 @@ load_dotenv()
 firebase_config = {"apiKey": "AIzaSyDe_xKKup4lVoPasLmAQW9Csc1zUzsxB0U","authDomain": "chuckwalla-69.firebaseapp.com",
   "databaseURL": "https://chuckwalla-69.firebaseio.com","storageBucket": "chuckwalla-69.appspot.com",
   "serviceAccount": json.loads(os.getenv("serviceAccountKeyJSON"))}
+  #"serviceAccount": "serviceAccountKey.json"}
 db = pyrebase.initialize_app(firebase_config).database()
