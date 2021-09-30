4180dec16ae28d90907016973ffe30c408f5d3d1
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-10-01 01:17:23 +0200

    custom pin message command because discord limits ughhh

---
+++ cogs/message_commands.py
@@ -0,0 +1,34 @@
+import disnake
+from disnake.ext import commands
+
+import yaml
+
+## Config Load ##
+pins_channel_id = yaml.safe_load(open('config.yml')).get('pins_channel_id')
+
+
+class MessageCommands(commands.Cog):
+    def __init__(self, client):
+        """Message commands."""
+        self.client = client
+
+
+    @commands.message_command(name="Pin this message")
+    async def pin(self, inter: disnake.ApplicationCommandInteraction):
+        # No repins under my watch
+        if inter.channel_id == pins_channel_id:
+            return await inter.response.send_message("Pinning a message in the pins channel. You must be a very special snowflake..", ephemeral=True)
+
+        # Create our View and add the profile link button
+        # prettier than using mentions imo
+        view = disnake.ui.View()
+        view.add_item(disnake.ui.Button(label=f"{inter.author.name}#{inter.author.discriminator}", url=f"discord://-/users/{inter.author.id}"))
+
+        msg = '\n'.join([attachment.url for attachment in inter.target.attachments])
+
+        pinned = await self.client.get_channel(pins_channel_id).send(msg, view=view)
+        await inter.response.send_message("Successfully pinned that message!", ephemeral=True)
+
+
+def setup(client):
+    client.add_cog(MessageCommands(client))
