8f83ced48f97926fef244bb9f171b27a134ce862
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-10-01 01:35:09 +0200

    now handles messages without attachments

--- cogs/message_commands.py
+++ cogs/message_commands.py
@@ -19,11 +19,17 @@ class MessageCommands(commands.Cog):
         if inter.channel_id == pins_channel_id:
             return await inter.response.send_message("Pinning a message in the pins channel. You must be a very special snowflake..", ephemeral=True)
 
+        # Message doesn't have to have attachments
+        if not inter.target.attachments:
+            return await inter.response.send_message("Only pin the funni images / videos / gifs", ephemeral=True)
+
+
         # Create our View and add the profile link button
         # prettier than using mentions imo
         view = disnake.ui.View()
         view.add_item(disnake.ui.Button(label=f"{inter.author.name}#{inter.author.discriminator}", url=f"discord://-/users/{inter.author.id}"))
 
+        # Create the message by appending all the links together
         msg = '\n'.join([attachment.url for attachment in inter.target.attachments])
 
         pinned = await self.client.get_channel(pins_channel_id).send(msg, view=view)
