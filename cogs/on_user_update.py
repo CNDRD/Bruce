b7857f617b843af6084af63383290388e1bd4fe8
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-10-05 17:26:49 +0200

    new library, new way to get avatar..

--- cogs/on_user_update.py
+++ cogs/on_user_update.py
@@ -3,18 +3,16 @@ from func.firebase_init import db
 from disnake.ext import commands
 
 
-
 class OnUserUpdate(commands.Cog):
     def __init__(self, client):
         """User updates their username or avatar, we update that in the database."""
         self.client = client
 
-
     @commands.Cog.listener()
     async def on_user_update(self, before, after):
         data = {
             "username": str(after),
-            "avatar_url":str(after.avatar_url_as(size=4096))
+            "avatar_url": str(after.display_avatar.with_size(4096))
         }
         db.child('users').child(after.id).update(data)
 
