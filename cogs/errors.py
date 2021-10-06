6e4d5bd0cb38a37da06cd0850ef71c8d952b4ec8
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-10-06 18:13:13 +0200

    match case instead of a bazillion isinstance()s

--- cogs/errors.py
+++ cogs/errors.py
@@ -9,30 +9,31 @@ class Errors(commands.Cog):
     @commands.Cog.listener()
     async def on_slash_command_error(self, inter, error):
         # All errors under "CheckFailure" (https://disnake.readthedocs.io/en/latest/ext/commands/api.html#exception-hierarchy)
-        if isinstance(error, commands.CheckAnyFailure):
-            message = "Some check failed. That means you're probably missing permissions to do this.."
-        elif isinstance(error, commands.PrivateMessageOnly):
-            message = "This command can only be used in PM's"
-        elif isinstance(error, commands.NoPrivateMessage):
-            message = "This command cannot be used in PM's"
-        elif isinstance(error, commands.NotOwner):
-            message = "You need to be the owner of this bot to use this command.."
-        elif isinstance(error, commands.MissingPermissions):
-            message = "You're missing permissions to do this."
-        elif isinstance(error, commands.BotMissingPermissions):
-            message = "I am missing permissions to do this."
-        elif isinstance(error, commands.MissingRole):
-            message = "You're missing a role to do this."
-        elif isinstance(error, commands.BotMissingRole):
-            message = "I am missing a role to do this."
-        elif isinstance(error, commands.MissingAnyRole):
-            message = "You're missing a role to do this."
-        elif isinstance(error, commands.BotMissingAnyRole):
-            message = "I am missing a role to do this."
-        elif isinstance(error, commands.NSFWChannelRequired):
-            message = "This command can only be used in a NSFW channel."
-        else:
-            message = "This is an unexpected error. Please notify someone about this if the issue persists."
+        match error:
+            case commands.CheckAnyFailure:
+                message = "Some check failed. That means you're probably missing permissions to do this.."
+            case commands.PrivateMessageOnly:
+                message = "This command can only be used in PM's"
+            case commands.NoPrivateMessage:
+                message = "This command cannot be used in PM's"
+            case commands.NotOwner:
+                message = "You need to be the owner of this bot to use this command.."
+            case commands.MissingPermissions:
+                message = "You're missing permissions to do this."
+            case commands.BotMissingPermissions:
+                message = "I am missing permissions to do this."
+            case commands.MissingRole:
+                message = "You're missing a role to do this."
+            case commands.BotMissingRole:
+                message = "I am missing a role to do this."
+            case commands.MissingAnyRole:
+                message = "You're missing a role to do this."
+            case commands.BotMissingAnyRole:
+                message = "I am missing a role to do this."
+            case commands.NSFWChannelRequired:
+                message = "This command can only be used in a NSFW channel."
+            case _:
+                message = "This is an unexpected error. Please notify someone about this if the issue persists."
 
         await inter.response.send_message(message, ephemeral=True)
 
