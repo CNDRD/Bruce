107b974bb3522248e57b5a05632f26dce2340897
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-10-06 02:00:55 +0200

    now taking random numbers from random.org :)

--- cogs/cicina.py
+++ cogs/cicina.py
@@ -1,5 +1,5 @@
+from func.cicina import get_cicina_today, get_random_cicina
 from func.firebase_init import db
-from func.cicina import get_cicina_today
 
 import disnake
 from disnake.ext.commands import Param
@@ -7,90 +7,93 @@ from disnake.ext import commands
 
 from datetime import datetime
 from pytz import timezone
-import numpy as np
 import yaml
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-## Config Load ##
+random_org_api_key = os.getenv("RANDOM_ORG_API_KEY")
+
+# Config Load
 local_timezone = yaml.safe_load(open('config.yml')).get('local_timezone')
+
 
 class Cicina(commands.Cog):
     def __init__(self, client):
         """Cicina command."""
         self.client = client
 
-
     @commands.slash_command(name="cicina", description="Shows your cicina size")
     async def cicina(
             self,
             inter: disnake.ApplicationCommandInteraction,
             top: str = Param(None, desc="Shows top 30 cicina users for today")
-        ):
+    ):
         uid = inter.author.id
         today = datetime.now(timezone(local_timezone)).strftime('%Y-%m-%d')
-        cicina = np.random.randint(0,51)
+        cicina = get_random_cicina(random_org_api_key)
 
         # Cicina Top
         if top is not None and top.lower() == "top":
-            cicinaToday = db.child('cicinaToday').get().val() or None
-            listOfToday = get_cicina_today(cicinaToday, today)
+            cicina_today = db.child('cicinaToday').get().val() or None
+            list_of_today = get_cicina_today(cicina_today, today)
 
-            if cicinaToday is None or listOfToday is None:
+            if cicina_today is None or list_of_today is None:
                 return await inter.response.send_message("Nobody claimed their cicina today")
 
-            listOfTodaySorted = sorted(listOfToday, key = lambda x: x['cicina'], reverse=True)
+            list_of_today_sorted = sorted(list_of_today, key=lambda x: x['cicina'], reverse=True)
 
-            topMsg = "**Today's top cicina's:**\n"
-            peepCount = 0
-            for _ in listOfTodaySorted:
-                usr = self.client.get_user(int(listOfTodaySorted[peepCount]['uid']))
-                if not usr: continue
-                cic = listOfTodaySorted[peepCount]['cicina']
-                topMsg += f"**{peepCount+1}.** {usr.name} with ***{cic}*** *cm*\n"
-                peepCount += 1
-                if peepCount == 30: break
-            return await inter.response.send_message(topMsg)
+            top_msg = "**Today's top cicina's:**\n"
+            peep_count = 0
+            for _ in list_of_today_sorted:
+                usr = self.client.get_user(int(list_of_today_sorted[peep_count]['uid']))
+                if not usr:
+                    continue
+                cic = list_of_today_sorted[peep_count]['cicina']
+                top_msg += f"**{peep_count + 1}.** {usr.name} with ***{cic}*** *cm*\n"
+                peep_count += 1
+                if peep_count == 30:
+                    break
+            return await inter.response.send_message(top_msg)
 
+        cicina_last = db.child('users').child(uid).child('cicina_last').get().val() or 0
+        cicina_longest = db.child('users').child(uid).child('cicina_longest').get().val() or 0
 
-        cicinaLast = db.child('users').child(uid).child('cicina_last').get().val() or 0
-        cicinaLongest = db.child('users').child(uid).child('cicina_longest').get().val() or 0
-
-        if cicinaLast != today:
+        if cicina_last != today:
             emote = disnake.utils.get(self.client.emojis, name="resttHA")
             msg = f'Dĺžka tvojej ciciny je {cicina} centimetrov {emote}'
 
-            cicinaTodayData = {"uid": uid, "cicina": cicina, "date": today}
-            db.child('cicinaToday').child(uid).update(cicinaTodayData)
+            cicina_today_data = {"uid": uid, "cicina": cicina, "date": today}
+            db.child('cicinaToday').child(uid).update(cicina_today_data)
 
         else:
-            midnight_ts = int(datetime.now(timezone(local_timezone)).replace(hour=0, minute=0, second=0).timestamp() + 86400)
+            midnight_ts = int(
+                datetime.now(timezone(local_timezone)).replace(hour=0, minute=0, second=0).timestamp() + 86400)
             msg = f'Cicina sa ti resetuje zajtra (~<t:{midnight_ts}:R>)'
             return await inter.response.send_message(msg, ephemeral=True)
 
-
-        if cicina > cicinaLongest:
+        if cicina > cicina_longest:
             cicina_for_db = cicina
         else:
-            cicina_for_db = cicinaLongest
+            cicina_for_db = cicina_longest
 
+        cicina_avg = db.child('users').child(uid).child('cicina_avg').get().val()
+        cicina_count = db.child('users').child(uid).child('cicina_count').get().val()
 
-        cicinaAvg = db.child('users').child(uid).child('cicina_avg').get().val()
-        cicinaCount = db.child('users').child(uid).child('cicina_count').get().val()
-
-
-        if cicinaCount is None:
+        if cicina_count is None:
             new_count = 1
             new_avg = cicina
 
         else:
-            new_avg = (cicinaAvg * cicinaCount + cicina) / (cicinaCount + 1)
-            new_count = cicinaCount + 1
+            new_avg = (cicina_avg * cicina_count + cicina) / (cicina_count + 1)
+            new_count = cicina_count + 1
 
         data = {
-            'cicina_longest':cicina_for_db,
-            'cicina_last':today,
+            'cicina_longest': cicina_for_db,
+            'cicina_last': today,
 
-            'cicina_avg':new_avg,
-            'cicina_count':new_count}
+            'cicina_avg': new_avg,
+            'cicina_count': new_count}
 
         db.child('users').child(uid).update(data)
         await inter.response.send_message(msg)
--- func/cicina.py
+++ func/cicina.py
@@ -1,9 +1,28 @@
+import requests
+import json
+
+
 def get_cicina_today(today, today_date):
-    if today is None: return None
+    if today is None:
+        return None
+
     xd = []
     for u in today:
         if today[u]['date'] == today_date:
             today[u].pop('date')
             xd.append(today[u])
-    if xd == []: return None
+
+    if not xd:
+        return None
     return xd
+
+
+def get_random_cicina(api_key: str) -> int:
+
+    data = json.dumps({
+        "jsonrpc": "2.0", "method": "generateIntegers", 'id': 1,
+        "params": {"apiKey": api_key, "n": 1, "min": 0, "max": 50, "replacement": True}
+    })
+    headers = {'Content-type': 'application/json', 'Content-Length': '200', 'Accept': 'application/json'}
+    response = requests.post(url='https://api.random.org/json-rpc/2/invoke', data=data, headers=headers)
+    return response.json().get('result').get('random').get('data')[0]
