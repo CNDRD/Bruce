2d43cd72fae032c1563e0a88f974be0ec60c9a42
CNDRD <michal.mikulec.mm@gmail.com>
2021-10-25 04:35:21 +0200

    - yfinance & trading (for now.. thanks heroku 🤦‍♂️)

--- func/trading.py
+++
@@ -1,8 +0,0 @@
-def get_trading_buy_db(stock: str, amount: int, bought_at: float):
-    data = {
-        stock: {
-            "amount": amount,
-            "boughtAt": bought_at
-        }
-    }
-    return data
