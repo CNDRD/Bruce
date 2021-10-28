4a65fbd566844666f3b9d789b8ce0c5d6bf7237c
CNDRD <20823071+CNDRD@users.noreply.github.com>
2021-10-29 01:11:19 +0200

    better visuals (xd), can now view individual stonks (even if not owned)

--- cogs/trading.py
+++ cogs/trading.py
@@ -1,5 +1,4 @@
 from func.firebase_init import db
-from func.stuff import add_spaces
 from func.trading import get_current_price, get_multiple_prices, get_trading_buy_db
 
 from disnake.ext.commands import Param
@@ -26,14 +25,13 @@ class Trading(commands.Cog):
             self,
             inter: disnake.MessageInteraction,
             operation: Literal["Buy", "Sell", "View"] = Param(..., desc="Are you buying, selling or just wanna view your portfolio?"),
-            stock: str = Param(None, desc="Symbol for the stock you wish to trade (Required when buying or selling)"),
+            stock: str = Param(None, desc="Symbol for the stock you wish to trade (Required when buying or selling; optional when viewing)"),
             amount: int = Param(None, desc="How many stocks do you wish to buy/sell (Required only when buying)")
     ):
         await inter.response.defer()
 
         if stock is not None:
             stock = stock.upper()
-            stock = stock.replace('/', '-')
 
         user_money = db.child("users").child(inter.author.id).child("money").get().val()
         currently_owns = db.child("trading").child(inter.author.id).get().val() or None
@@ -48,15 +46,15 @@ class Trading(commands.Cog):
                 current_price = get_current_price(stock)
 
                 if current_price is None:
-                    return await inter.edit_original_message(content=f"Symbol `{stock}` either doesn't exist or I don't have access to it", view=AvailableTokens())
+                    return await inter.edit_original_message(content=f"Symbol *{stock}* either doesn't exist or I don't have access to it", view=AvailableTokens())
 
                 buying_cost = current_price * amount
                 if buying_cost > user_money:
-                    return await inter.edit_original_message(content=f"You don't have enough money for this ({add_spaces(int(buying_cost))} > {add_spaces(user_money)})")
+                    return await inter.edit_original_message(content=f"You don't have enough money for this ({int(buying_cost):,} > {user_money:,})".replace(',', ' '))
 
                 db.child("trading").child(inter.author.id).update(get_trading_buy_db(stock, amount, current_price))
                 db.child("users").child(inter.author.id).update({"money": int(user_money - buying_cost)})
-                return await inter.edit_original_message(content=f"Successfully bought *{add_spaces(amount)}* shares of **{stock.replace('-', '/')}** for *{add_spaces(int(buying_cost))}* shekels")
+                return await inter.edit_original_message(content=f"Successfully bought *{amount:,}* shares of **{stock}** for *{int(buying_cost):,}* shekels".replace(',', ' '))
 
             case 'Sell':
                 if currently_owns is None:
@@ -73,32 +71,60 @@ class Trading(commands.Cog):
                 current_price = get_current_price(stock)
                 selling_cost = current_price * amount_bought
 
-                profit_loss = "loss"
-                outcome_money = bought_cost - selling_cost
+                outcome_money = selling_cost - bought_cost
 
-                if selling_cost >= bought_cost:
-                    profit_loss = "profit"
-                    outcome_money = selling_cost - bought_cost
+                profit_loss = "profit" if outcome_money >= 0 else "loss"
 
                 db.child("trading").child(inter.author.id).child(stock).remove()
                 db.child("users").child(inter.author.id).update({"money": int(user_money + selling_cost)})
 
-                msg = f"Successfully sold *{amount_bought}* stocks of **{stock.replace('-', '/')}** for a {profit_loss} of `{add_spaces(int(outcome_money))}` shekels"
+                msg = f"Successfully sold *{amount_bought}* stocks of **{stock}** for a {profit_loss} of `{int(outcome_money):,}` shekels".replace(',', ' ')
                 return await inter.edit_original_message(content=msg)
 
             case "View":
                 if currently_owns is None:
                     return await inter.edit_original_message(content="You don't own any stocks!")
 
+                if stock is not None:
+                    current_price = get_current_price(stock)
+
+                    if stock not in currently_owns:
+                        if current_price is None:
+                            return await inter.edit_original_message(content=f"Symbol *{stock}* either doesn't exist or I don't have access to it", view=AvailableTokens())
+
+                        msg = f"**{stock}** - Current Price: **{current_price:,}**"
+                        return await inter.edit_original_message(content=msg.replace(',', ' '))
+
+                    bought_at = currently_owns.get(stock).get("boughtAt")
+                    amount_bought = currently_owns.get(stock).get("amount")
+                    bought_cost = bought_at * amount_bought
+                    selling_cost = current_price * amount_bought
+                    outcome_money = selling_cost - bought_cost
+
+                    current_price = round(current_price, 2) if current_price >= .01 else current_price
+                    selling_cost = round(selling_cost, 2) if selling_cost >= .01 else selling_cost
+                    bought_at = round(bought_at, 2) if bought_at >= .01 else bought_at
+                    bought_cost = round(bought_cost, 2) if bought_cost >= .01 else bought_cost
+
+                    position_worth = current_price * amount_bought
+                    position_worth = round(position_worth, 2) if position_worth >= .01 else position_worth
+
+                    msg = f"Your **{stock}** position is worth **{position_worth:,}** shekels\n" \
+                          f"Current Price: **{current_price:,}**\n" \
+                          f"Bought at: **{bought_at:,}** worth *{bought_cost:,}* shekels\n" \
+                          f"Profit: **{int(outcome_money):,}** | " \
+                          f"*{amount_bought:,} stock{'s' if amount_bought > 1 else ''}*\n\n"
+                    return await inter.edit_original_message(content=msg.replace(',', ' '))
+
                 msg = "__Your current stonks sir:__\n\n"
                 currently_owns_prices = get_multiple_prices(currently_owns)
                 total_profit = 0
 
                 for stonk in currently_owns:
-                    current_price = currently_owns_prices[stonk.replace('-', '/')]
+                    current_price = currently_owns_prices[stonk]
                     bought_price = currently_owns[stonk]['boughtAt']
                     amount_bought = currently_owns[stonk]['amount']
-                    profit = (current_price*amount_bought)-(bought_price*amount_bought)
+                    profit = (current_price * amount_bought) - (bought_price * amount_bought)
                     total_profit += profit
 
                     if current_price >= .01:
@@ -107,13 +133,11 @@ class Trading(commands.Cog):
                     if bought_price >= .01:
                         bought_price = round(bought_price, 2)
 
-                    msg += f"**{stonk.replace('-', '/')}** - " \
-                           f"Current Price: `{current_price:,}` | " \
-                           f"Bought at: `{bought_price:,}` | " \
-                           f"Profit: `{int(profit):,}` " \
-                           f"*({amount_bought:,} stock{'s' if amount_bought > 1 else ''})*\n"
+                    msg += f"**{stonk}** - Current Price: **{current_price:,}** | " \
+                           f"Bought at: **{bought_price:,}** | " \
+                           f"Profit: **{round(profit, 2):,}**\n"
 
-                msg += f"\n*Your total profit is `{int(total_profit):,}` shekels*"
+                msg += f"\n*Your total profit is **{int(total_profit):,}** shekels*"
 
                 return await inter.edit_original_message(content=msg.replace(',', ' '))
 
