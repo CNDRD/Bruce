dc8e0bfa7428e9596823bcbd5a3799b3cd447bfd
CNDRD <michal.mikulec.mm@gmail.com>
2022-02-02 23:55:30 +0100

    fix for bronze 5 rank image

--- func/r6.py
+++ func/r6.py
@@ -255,7 +255,7 @@ def get_rank(rank) -> str:
         "copper 3": "aNCvwAI",
         "copper 2": "fUzUApd",
         "copper 1": "eGuxE0k",
-        "bronze v": "bbjMf4V",
+        "bronze 5": "bbjMf4V",
         "bronze 4": "75IEQkD",
         "bronze 3": "GIt29R0",
         "bronze 2": "sTIXKlh",
