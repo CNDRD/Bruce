2f593f3e24aec148b7e792e6353d23cee5336cf9
Michal Mikulec <20823071+CNDRD@users.noreply.github.com>
2022-02-21 13:46:42 +0100

    simplify and rework how nfts are shown, decrease price

--- cogs/nft.py
+++ cogs/nft.py
@@ -6,15 +6,10 @@ import time
 
 
 class NFTsLink(disnake.ui.View):
-    def __init__(self, ids: list[str] | str):
+    def __init__(self, uid: int):
         super().__init__()
 
-        if isinstance(ids, list):
-            url = f"https://diskito.eu/NFT?ids={','.join([nft for nft in ids])}"
-            self.add_item(disnake.ui.Button(label="Your NFT collection sir", url=url))
-        else:
-            url = f"https://diskito.eu/NFT?ids={ids}"
-            self.add_item(disnake.ui.Button(label="Enjoy!", url=url))
+        self.add_item(disnake.ui.Button(label="Your NFT collection sir", url=f"https://diskito.eu/NFT?id={uid}"))
 
 
 class NFT(commands.Cog):
@@ -22,16 +17,16 @@ class NFT(commands.Cog):
         """NFTs lmao."""
         self.client = client
 
-    @commands.slash_command(name="nft", description="Creates a link to view all of your Diskíto NFT's")
+    @commands.slash_command(
+        name="nft",
+        description="Creates a link to view all of your Diskíto NFT's"
+    )
     async def _nft(self, inter: disnake.MessageInteraction):
         user_nfts = db.child("NFT").child("owned").child(inter.author.id).get().val() or None
         if user_nfts is None:
             return await inter.response.send_message("You do not own any NFT's! Go buy some, it's great!!", ephemeral=True)
 
-        # Converting a nested OrderedDict to a list of strings..
-        # Respectfully yoinked from https://stackoverflow.com/q/16228248/13186339
-        nft_list = list(map(lambda x: x["id"], [*dict(user_nfts).values()]))
-        return await inter.response.send_message("Here you go:", view=NFTsLink(nft_list))
+        return await inter.response.send_message("Here you go:", view=NFTsLink(inter.author.id))
 
     @commands.slash_command(
         name="mint",
@@ -39,7 +34,7 @@ class NFT(commands.Cog):
     )
     async def _mint(self, inter: disnake.MessageInteraction):
         buyer_money = db.child("users").child(inter.author.id).child("money").get().val()
-        if buyer_money <= 100_000:
+        if buyer_money <= 10_000:
             return await inter.response.send_message("You do not have enough shekels to mint a new NFT!", ephemeral=True)
 
         minted = db.child("NFT").child("ids").get()
@@ -51,7 +46,7 @@ class NFT(commands.Cog):
         db.child("users").child(inter.author.id).update({"money": int(buyer_money - 100_000)})
         db.child("NFT").child("ids").child(mint_key).remove()
         db.child("NFT").child("owned").child(inter.author.id).update({mint_id: {"boughtAt": time.time(), "id": mint_id}})
-        return await inter.response.send_message("Your new NFT:", view=NFTsLink(mint_id))
+        return await inter.response.send_message("Your new NFT:", view=NFTsLink(inter.author.id))
 
 
 def setup(client):
