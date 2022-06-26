040974d9c861e10e066962b4d6fdf5df836524c4
CNDRD <michal.mikulec.mm@gmail.com>
2022-06-26 12:35:59 +0200

    replace dumb shit by built in beauty

--- func/stuff.py
+++ func/stuff.py
@@ -3,13 +3,8 @@ def ordinal(n) -> str:
 # https://stackoverflow.com/a/20007730
 
 
-def add_spaces(numero) -> str:
-    numero = ''.join(reversed(str(numero)))
-    a = [numero[i:i + 3] for i in range(0, len(numero), 3)]
-    a = ' '.join([numero[i:i + 3] for i in range(0, len(numero), 3)])
-    a = ''.join(reversed(str(a)))
-    return a
-# https://stackoverflow.com/a/15254225/13186339
+def add_spaces(numero: int, char: str = " ") -> str:
+    return f"{numero:,}".replace(",", char)
 
 
 def user_has_role(ctx, role_check) -> bool:
